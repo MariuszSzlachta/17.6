a902a3999852c270ef5577d2bc6c4b9d5cd6b023
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-07-23 14:16:20 +0200

    Fix problem with lack of vp-tag and add solutions for issue-1

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ app.set('view engine', 'pug');
 app.set('views','./views');
 
 app.get('/', function(req, res){
-  res.render('site')
-})
+  res.render('site');
+});
 
 app.get('/auth/google', function(req, res) {
-  res.render('content')
-})
+  res.render('content', {username: req.query.username});
+});
 
 
 app.listen(3000);
   
 app.use(function(req, res, next){
-  console.log('404')
+  console.log('404');
   res.status(404).send('Sorry but we couldn\'t find what you need');
-})
+});
